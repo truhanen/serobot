ee1e39eebcf352a4040d3e30addae531c4ef6fed
Tuukka Ruhanen <tuukka.t.ruhanen@gmail.com>
2020-04-16 13:00:15 +0300

    Move files before setting up two namespace packages

---
+++ truhanen.serobot.web/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup, find_namespace_packages
+
+
+# TODO Optionally build the web frontend?
+
+
+setup(
+    name='truhanen.serobot',
+    version='0.1.0',
+    author='Tuukka Ruhanen',
+    author_email='tuukka.t.ruhanen@gmail.com',
+    description='API & web UI for controlling a Raspberry Pi robot.',
+    install_requires=[
+        # Hardware control
+        'psutil',
+        'smbus',
+        'RPi.GPIO',
+        'picamera',
+        'rpi-ws281x',
+        # Web server
+        'aiohttp',
+        'aiohttp_security',
+        'aiohttp_session',
+        'cryptography',
+    ],
+    packages=find_namespace_packages(),
+    scripts=[
+        'scripts/start_serobot_server',
+    ],
+    zip_safe=False,
+)
